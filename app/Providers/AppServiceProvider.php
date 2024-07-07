c4b0941a7212f44a286bffef3e24393d0cad8f8f
Ing-art <62476397+Ing-art@users.noreply.github.com>
2024-07-08 00:27:19 +0200

    2024-07-07 Template

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //So we can use Bootsrap instead of Tailwind
+        Paginator::useBootstrap();
     }
 }
+
